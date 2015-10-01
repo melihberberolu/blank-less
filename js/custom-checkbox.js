c71cbc3f9afa95f6024e181926d4c2d7b7a38462
eomerx <omer@aslanbakan.com>
2015-10-01 10:40:39 +0300

    custom checkboxes modiify

--- js/custom-checkbox.js
+++ js/custom-checkbox.js
@@ -1,27 +1,29 @@
-var setIdForInputs = function(elementClassName) {
-  var checkboxes = document.getElementsByClassName(elementClassName);
-  console.log('checkboxes length' , checkboxes.length);
-  for (var i = 0; i < checkboxes.length; i++) {
-    var input = checkboxes[i].querySelector("input");
-    console.log('input ' , input);
-    var label = checkboxes[i].querySelector("label");
-    var isId = input.id;
-    console.log('isId ' , isId);
-    var isFor = label.for;
-    console.log('isFor ' , isFor);
-    if (!isId) {
-      console.log('if 1');
-      var uniqueId = "input-" + Math.random().toString(32).slice(2);
-      console.log('uniqueId ' , uniqueId);
-      label.setAttribute("for", uniqueId);
-      input.id = uniqueId;
+(function(window, document, undefined) {
+  var setIdForInputs = function(elementClassName) {
+    var checkboxes = document.getElementsByClassName(elementClassName);
+    // console.log('checkboxes length' , checkboxes.length);
+    for (var i = 0; i < checkboxes.length; i++) {
+      var input = checkboxes[i].querySelector("input");
+      // console.log('input ' , input);
+      var label = checkboxes[i].querySelector("label");
+      var isId = input.id;
+      // console.log('isId ' , isId);
+      var isFor = label.for;
+      // console.log('isFor ' , isFor);
+      if (!isId) {
+        // console.log('if 1');
+        var uniqueId = "input-" + Math.random().toString(32).slice(2);
+        // console.log('uniqueId ' , uniqueId);
+        label.setAttribute("for", uniqueId);
+        input.id = uniqueId;
+      }
+      else {
+        label.setAttribute("for", isId);
+      }
     }
-    else {
-      label.setAttribute("for", isId);
-    }
-  };
-}
-window.onload = function() {
-  setIdForInputs("checkbox-custom");
-  setIdForInputs("radio-custom");
-}
+  }
+  window.onload = function() {
+    setIdForInputs("checkbox-custom");
+    setIdForInputs("radio-custom");
+  }
+})(window, document);
