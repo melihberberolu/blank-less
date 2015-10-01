c37d91be0eb5756bc479e038008cc7fbec72571c
eomerx <omer@aslanbakan.com>
2015-10-01 14:59:37 +0300

    window print size function converted to pure javascript

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
 //=============================================
 //           JQUERY DOCUMENT READY            =
 //=============================================
-$("document").ready(function() {
+$(document).ready(function() {
 
 
 
@@ -181,72 +181,71 @@ $(window).load(function() {
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
+(function() {
 
-
-//=============================================
-//              PRINT WINDOW SIZE             =
-//=============================================
-var windowSizePrint = function(argument) {
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
-  if (!$("#window-size-info").length) {
-    $('body').append('<div id="window-size-info"><div><span class="w"></span> X <span class="h"></span></div><style type="text/css">#window-size-info{z-index:9999999;position:fixed;right:0px;bottom:30px;padding:3px 8px;font-family:"Open Sans","Lato",Tahoma;font-size:15px;color:#fff;background-color:#000;box-shadow:0 0 0 2px#fff;}</style></div>');
+  //=============================================
+  //              PRINT WINDOW SIZE             =
+  //=============================================
+  var isButtonInsertedToBody;
+  var windowSizePrint = function() {
+    if (!isButtonInsertedToBody) {
+      document.body.insertAdjacentHTML("beforeend", '<div id="window-size-info"><div><span class="w"></span> X <span class="h"></span></div><style type="text/css">#window-size-info{z-index:9999999;position:fixed;right:0px;bottom:30px;padding:3px 8px;font-family:"Open Sans","Lato",Tahoma;font-size:15px;color:#fff;background-color:#000;box-shadow:0 0 0 2px#fff;}</style></div>');
+      isButtonInsertedToBody = true;
+    }
+    document.getElementById("window-size-info").querySelector(".w").innerHTML = window.innerWidth;
+    document.getElementById("window-size-info").querySelector(".h").innerHTML = window.innerHeight;
   }
-  $('#window-size-info span.w').text(windowWidth);
-  $('#window-size-info span.h').text(windowHeight);
-}
-$(document).ready(windowSizePrint);
-$(window).load(windowSizePrint);
-$(window).resize(windowSizePrint);
-//-------  End of print window size  --------//
+  document.addEventListener("DOMContentLoaded", windowSizePrint);
+  window.addEventListener("resize", windowSizePrint);
+  //-------  End of print window size  --------//
 
-//////////////////////
-// browser function //
-//////////////////////
-var nua = navigator.userAgent;
-window.nua = nua;
-var browser = browser || {};
-browser = {
-  nua            : nua,
-  html           : $('html').attr('class'),
-  webkit         : /webkit/gi.test(nua),
-  gecko          : /gecko/gi.test(nua),
-  mobile         : /Mobile/gi.test(nua),
-  windows        : /Windows/gi.test(nua),
-  mac            : /Mac OS/gi.test(nua),
-  chrome         : /Chrome/gi.test(nua),
-  firefox        : /Firefox/gi.test(nua),
-  apple          : /Safari/gi.test(nua),
-  ipod           : /iPod/gi.test(nua),
-  iphone         : /iPhone/gi.test(nua),
-  ipad           : /iPad/gi.test(nua),
-  android        : /Android/gi.test(nua),
-  androidVersion : /Android/gi.test(nua) ? nua.match(/Android ([0-9\.]+);/)[1] : "",
-  ie             : /MSIE/gi.test(nua),
-  ie11           : /MSIE 11/gi.test(nua),
-  ie10           : /MSIE 10/gi.test(nua),
-  ie9            : /MSIE 9/gi.test(nua),
-  ie8            : /MSIE 8/gi.test(nua),
-  ie7            : /MSIE 7/gi.test(nua),
-  ie6            : /MSIE 6/gi.test(nua),
-  operaMini      : /Opera Mini/gi.test(nua),
-  ucBrowser      : /UCBrowser/gi.test(nua),
-  x64            : /WOW64/gi.test(nua),
-  x86            : /WOW86/gi.test(nua),
-  tablet         : $(window).width() >= 768 && /Mobile/gi.test(nua),
-  desktop        : $(window).width() >= 768 && !/Mobile/gi.test(nua),
-  phone          : $(window).width() < 768 && /Mobile/gi.test(nua),
-};
-window.browser = browser;
-console.groupCollapsed("browser variables");
-var browserVariables = "";
-for (var prop in browser) {
-  if (browser.hasOwnProperty(prop)) {
-    console.log(prop + " : " + browser[prop]);
-    browserVariables += prop + " : " + browser[prop] + "\n";
+  //////////////////////
+  // browser function //
+  //////////////////////
+  var browser = {};
+  window.browser = browser;
+  browser.nua = navigator.userAgent;
+  browser = {
+    userAgent      : navigator.userAgent,
+    html           : document.documentElement.classList,
+    webkit         : /webkit/gi.test(this.nua),
+    gecko          : /gecko/gi.test(this.nua),
+    mobile         : /Mobile/gi.test(this.nua),
+    windows        : /Windows/gi.test(this.nua),
+    mac            : /Mac OS/gi.test(this.nua),
+    chrome         : /Chrome/gi.test(this.nua),
+    firefox        : /Firefox/gi.test(this.nua),
+    apple          : /Safari/gi.test(this.nua),
+    ipod           : /iPod/gi.test(this.nua),
+    iphone         : /iPhone/gi.test(this.nua),
+    ipad           : /iPad/gi.test(this.nua),
+    android        : /Android/gi.test(this.nua),
+    androidVersion : /Android/gi.test(this.nua) ? this.nua.match(/Android ([0-9\.]+);/)[1] : "",
+    ie             : /MSIE/gi.test(this.nua),
+    ie11           : /MSIE 11/gi.test(this.nua),
+    ie10           : /MSIE 10/gi.test(this.nua),
+    ie9            : /MSIE 9/gi.test(this.nua),
+    ie8            : /MSIE 8/gi.test(this.nua),
+    ie7            : /MSIE 7/gi.test(this.nua),
+    ie6            : /MSIE 6/gi.test(this.nua),
+    operaMini      : /Opera Mini/gi.test(this.nua),
+    ucBrowser      : /UCBrowser/gi.test(this.nua),
+    x64            : /WOW64/gi.test(this.nua),
+    x86            : /WOW86/gi.test(this.nua),
+    tablet         : window.outerWidth >= 768 && /Mobile/gi.test(this.nua),
+    desktop        : window.outerWidth >= 768 && !/Mobile/gi.test(this.nua),
+    phone          : window.outerWidth < 768 && /Mobile/gi.test(this.nua),
+  };
+  console.groupCollapsed("browser variables");
+  var browserVariables = "";
+  for (var prop in browser) {
+    if (browser.hasOwnProperty(prop)) {
+      console.log(prop + " : " + browser[prop]);
+      browserVariables += prop + " : " + browser[prop] + "\n";
+    }
   }
-}
-console.groupEnd();
-$(function() {
-  $("#window-size-info > div").attr("title", browserVariables);
-});
+  console.groupEnd();
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("window-size-info").setAttribute("title", browserVariables);
+  });
+})();
