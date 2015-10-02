9c56f532a69fe88bf412e266973f58b63c283344
eomerx <omer@aslanbakan.com>
2015-10-02 13:04:36 +0300

    linters and small fixes

--- js/custom-checkbox.js
+++ js/custom-checkbox.js
@@ -1,19 +1,19 @@
 (function(window, document, undefined) {
   var setIdForInputs = function(elementClassName) {
     var elements = document.getElementsByClassName(elementClassName);
-    console.log('elements length' , elements.length);
+    // console.log('elements length' , elements.length);
     for (var i = 0; i < elements.length; i++) {
       var input = elements[i].querySelector("input");
-      console.log('input ' , input);
+      // console.log('input ' , input);
       var label = elements[i].querySelector("label");
       var isId = input.id;
-      console.log('isId ' , isId);
+      // console.log('isId ' , isId);
       var isFor = label.for;
-      console.log('isFor ' , isFor);
+      // console.log('isFor ' , isFor);
       if (!isId) {
-        console.log('if 1');
+        // console.log('if 1');
         var uniqueId = "input-" + Math.random().toString(36).slice(2);
-        console.log('uniqueId ' , uniqueId);
+        // console.log('uniqueId ' , uniqueId);
         label.setAttribute("for", uniqueId);
         input.id = uniqueId;
       }
